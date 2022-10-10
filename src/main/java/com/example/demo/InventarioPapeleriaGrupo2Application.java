67f54e269c966b1c075999f79c5d2e77c99c4a4c
andkain <andke1462@gmail.com>
2022-10-09 21:45:57 -0500

    eureka

---
+++ src/main/java/com/example/demo/InventarioPapeleriaGrupo2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioPapeleriaGrupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioPapeleriaGrupo2Application.class, args);
+	}
+
+}
